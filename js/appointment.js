ceefdeeda0a7d62002d3d2920ba3904af3e5e407
Ragnora <as310103@gmail.com>
2023-10-07 15:50:12 +0700

    Fetch

---
+++ js/appointment.js
@@ -0,0 +1,96 @@
+// Close popup
+function closeButton() {
+    const popup = document.querySelector('.popup');
+    const close = document.querySelector('.close');
+    close.addEventListener('click', () => {
+        popup.style.display = 'none';
+    })
+}
+
+
+const userTable = document.getElementById('tbody');
+
+fetch('https://be-balikpapan-30-production.up.railway.app/appointment')
+.then(response => response.json())
+.then(data => {
+    for (let i = 0; i < data.data.length; i++) {
+        const tabelRow = document.createElement('tr');
+        tabelRow.setAttribute('class', 'userRow');
+        userTable.appendChild(tabelRow);
+        const userRow = document.getElementsByClassName('userRow');
+        userRow[i].innerHTML = `
+        <td>${i + 1}</td>
+        <td>${data.data[i].nama}</td>
+        <td>${data.data[i].umur}</td>
+        <td>${data.data[i].doctor.nama}</td>
+        <td>${data.data[i].reason}</td>
+        <td><img class="detail" data-id="${data.data[i].id}" src="../Asset/lihat.png" alt=""></td>
+        `
+        detailAppointment()
+    }
+
+})
+.catch(error => {
+    console.log(error);
+})
+
+
+
+
+// Detail Appointment
+const infoAppointment = document.querySelector('.infoAppointment');
+function detailAppointment() {
+    const lihatAppointment = document.querySelectorAll('.detail');
+    lihatAppointment.forEach((e) => {
+        e.addEventListener('click', function() {
+            // Pop Up
+            const popup = document.querySelector('.popup');
+            popup.style.display = 'block';
+            closeButton()
+
+            // Get Id
+            const dataId = this.getAttribute('data-id');
+            
+            // Data
+            fetch(`https://be-balikpapan-30-production.up.railway.app/appointment/${dataId}`)
+            .then(response => response.json())
+            .then(data => {
+                const Data = data.data;
+                infoAppointment.innerHTML = `
+                <div class="detailInfoAppointment">
+                    <h4>Date</h4>
+                    <p>: ${Data.date}</p>
+                </div>
+                <div class="detailInfoAppointment">
+                    <h4>Name</h4>
+                    <p>: ${Data.nama}</p>
+                </div>  
+                <div class="detailInfoAppointment">
+                    <h4>Age</h4>
+                    <p>: ${Data.umur}</p>
+                </div>
+                <div class="detailInfoAppointment">
+                    <h4>Gender</h4>
+                    <p>: ${Data.gender}</p>
+                </div>
+                <div class="detailInfoAppointment">
+                    <h4>Phone</h4>
+                    <p>: ${Data.phone}</p>
+                </div>
+                <div class="detailInfoAppointment">
+                    <h4>Specialist</h4>
+                    <p>: ${Data.doctor.specialist}</p>
+                </div>
+                <div class="detailInfoAppointment">
+                    <h4>Doctor</h4>
+                    <p>: ${Data.doctor.nama}</p>
+                </div>
+                <div class="detailInfoAppointment">
+                    <h4>Reason</h4>
+                    <p>: ${Data.reason}</p>
+                </div>
+                `
+            })
+        })
+    })
+}
